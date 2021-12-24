5d23e5a4403fff07f2f475ec6e1ea9c7b9c88ae1
Agustin Mediotti <70532867+Agustin-Mediotti@users.noreply.github.com>
2021-12-24 16:23:52 -0300

    Fixed: collision

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ const player = document.getElementById("player");
 const obstacle_sm = document.getElementById("obstacle_small");
 const playerMove = document.getElementById("playerImg");
 const buttonPlayStop = document.getElementById("buttonPlayStop");
+const btn_restart = document.getElementById("btn_restart");
 const backgroundSky = document.getElementById("sky");
 const backgroundGrass = document.getElementById("grass");
 const board = document.getElementById("board");
@@ -13,7 +14,7 @@ const hitSound = document.querySelector("#hit");
 music.loop = true;
 
 let scoreInterval;
-let score = 10.000;
+let score = 10;
 
 
 
@@ -68,6 +69,7 @@ function resumeAnimation() {
 }
 
 function resumeGame() {
+    obstacle_sm.classList.add("obstaclesMovement");
     resumeAnimation();
 }
 
@@ -81,6 +83,10 @@ function resumeScore() {
         document.getElementById("score").innerText = score;
     },20000);
 }
+
+btn_restart.addEventListener('click',() => {
+    restartGame();
+})
 
 buttonPlayStop.addEventListener('click', () => {
 
@@ -101,17 +107,25 @@ pauseGame();
 function restartGame() {
     resetScore();
     removeJump();
+    obstacle_sm.classList.remove("obstaclesMovement");
+    player.classList.remove("hit");
+    player.classList.add("playerMove");
+    resumeGame();
+    hitSound.pause();
+
+    if(buttonPlayStop.classList.contains("play")) {
+        buttonPlayStop.classList.toggle("play");
+    }
 }
 
 function resetScore() {
-    score = 0;
+    score = 10;
     document.getElementById("score").innerText = score;
 }
 
 // COLLISIONS
 setInterval(function(){
-    if(player.getBoundingClientRect().right >= obstacle_sm.getBoundingClientRect().left && player.getBoundingClientRect().bottom >= obstacle_sm.getBoundingClientRect().top){
-
+    if(player.getBoundingClientRect().right >= obstacle_sm.getBoundingClientRect().left && player.getBoundingClientRect().bottom >= obstacle_sm.getBoundingClientRect().top) {
 
         obstacle_sm.style.animationPlayState = 'paused';
         backgroundSky.style.animationPlayState = 'paused';
@@ -127,3 +141,9 @@ setInterval(function(){
         player.classList.add("hit");
     }
 }, 500)
+
+
+if (score < 9) {
+    pauseGame();
+    alert("Thanks for Playing! Work on progress");
+}
